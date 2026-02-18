aad4c69b66317ee69e74674fe3484e649209e797
ajamol2197 <ajamol2197@gmail.com>
2026-02-18 16:54:30 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# 1
+student = {"Ali": 85,
+           "Vali": 90,
+           "Hasan": 78}
+print(student.values())
+
+# 2
+student = {"Ali": 85,
+           "Vali": 90,
+           "Hasan": 78}
+
+print(max(student))
+
+# 3
+prices = {"apple": 5000,
+          "banana": 3000,
+          "cherry": 7000}
+print(prices.values())
+rez = sum(prices.values())
+print(rez)
+
+# 4
+car = {"brand":
+       "Chevrolet",
+       "year": 2022}
+print(car)
+
+car = {"color"} = "back"
+print(car)
+
+# 5
+person = {"name": "Ali",
+          "age": 25,
+          "city": "Toshkent"}
+print(person)
+
+del person["age"]
+print(person)
